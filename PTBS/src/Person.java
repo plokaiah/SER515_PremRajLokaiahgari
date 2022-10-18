3a8dbfb565393c7f162b0925673df97e4247d022
plokaiah <plokaiah@asu.edu>
2022-10-18 12:07:21 -0700

    Added initial files

---
+++ PTBS/src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+	private ProductMenu theProductMenu;
+
+	public void showLabels() {
+
+	}
+
+	public ProductMenu CreateProductMenu() {
+		return null;
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showMenu() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+}
---
+++ PTBS/src/ProductMenu.java
@@ -0,0 +1,15 @@
+public interface ProductMenu {
+
+	public void showMenu();
+
+	public void showAddButton();
+
+	public void showViewButton();
+
+	public void showRadioButton();
+
+	public abstract void showLabels();
+
+	public abstract void showComboxes();
+
+}
